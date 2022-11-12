02f8ada995342392943c35a0abf1f6d75c9ff0b9
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-11-12 18:22:25 +0600

    added react router , tailwind, daisyUI, assets

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          primary: "#0FCFEC",
+
+          secondary: "#19D3AE",
+
+          accent: "#3A4256",
+
+          neutral: "#252239",
+
+          "base-100": "#F3F0F5",
+
+          info: "#A1CFE3",
+
+          success: "#2EB8A1",
+
+          warning: "#EFD06C",
+
+          error: "#E33B43",
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
