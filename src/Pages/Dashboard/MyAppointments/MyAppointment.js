1f3140e5ec45337cc26b52ee1ebf26a54d0317fa
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-11-21 17:28:03 +0600

    added myAppointment for user

--- src/Pages/Dashboard/MyAppointments/MyAppointment.js
+++ src/Pages/Dashboard/MyAppointments/MyAppointment.js
@@ -1,19 +1,25 @@
 import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
+import Spinner from '../../../components/Spinner/Spinner';
 import { AuthContext } from '../../../context/AuthProvider';
+import { TbEdit } from "react-icons/tb";
+import { ImCancelCircle } from "react-icons/im";
 
 const MyAppointment = () => {
     const { user } = useContext(AuthContext)
     const url = `http://localhost:5000/bookings?email=${user?.email}`;
-    const { data: bookings = [] } = useQuery({
-        queryKey: ['bookings' , user?.email],
-        queryFn : async () => {
+    const { data: bookings = [] , isLoading} = useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: async () => {
             const res = await fetch(url)
             const data = await res.json()
             return data
         }
     })
-    console.log(bookings)
+    if (isLoading){
+        return <Spinner></Spinner>
+    }
+    // console.log(bookings)
     return (
         <div>
             <h3 className="text-3xl mb-5">My Appointments</h3>
@@ -26,11 +32,22 @@ const MyAppointment = () => {
                             <th>Treatment</th>
                             <th>Date</th>
                             <th>Time</th>
+                            <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            bookings.map(booking => <p>Hello</p>)
+                            bookings.map((booking, i) => <tr key={booking._id}>
+                                <th>{i + 1}</th>
+                                <td>{booking.patient}</td>
+                                <td>{booking.careFor}</td>
+                                <td>{booking.appointmentDate}</td>
+                                <td>{booking.slot}</td>
+                                {/* <td><TbEdit className='btn btn-xs btn-outline btn-primary'></TbEdit></td> */}
+                                <td><button><TbEdit></TbEdit></button></td>
+                                <td><button><ImCancelCircle></ImCancelCircle></button></td>
+                            </tr>)
                         }
                     </tbody>
                 </table>
--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Spinner = () => {
     return (
-        <div class="flex justify-center items-center">
-            <div class="spinner-border border-primary text-white  animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
-                <span class="visually-hidden">...</span>
+        <div className="flex justify-center items-center">
+            <div className="spinner-border border-primary text-white  animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
+                <span className="visually-hidden">...</span>
             </div>
         </div>
     );
